d80c254b6d55d35179564d08408119aff3bb5b72
Haena <gogohaena@gmail.com>
2024-11-07 14:32:31 +0900

    first commit

---
+++ src/main/java/com/sparta/nanglangeats/NanglangEatsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nanglangeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanglangEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NanglangEatsApplication.class, args);
+    }
+
+}
