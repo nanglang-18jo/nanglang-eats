3b65dd7c18f4d90e5c1dcc385dff91f83afc39a8
MinJeong LEE <alswjd6807@naver.com>
2024-11-12 04:16:55 +0900

    [feat] 가게 등록

--- src/main/java/com/sparta/nanglangeats/domain/store/entity/StoreImage.java
+++ src/main/java/com/sparta/nanglangeats/domain/store/entity/StoreImage.java
@@ -1,5 +1,8 @@
 package com.sparta.nanglangeats.domain.store.entity;
 
+import com.sparta.nanglangeats.domain.image.service.dto.ImageResponse;
+import com.sparta.nanglangeats.global.common.entity.Timestamped;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -18,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "p_store_image")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class StoreImage {
+public class StoreImage extends Timestamped {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -34,9 +37,16 @@ public class StoreImage {
 	private String url;
 
 	@Builder
-	public StoreImage(Store store, String fileName, String url) {
+	public StoreImage(Store store, ImageResponse response) {
 		this.store = store;
-		this.fileName = fileName;
-		this.url = url;
+		this.fileName = response.getFileName();
+		this.url = response.getUrl();
+	}
+
+	/**
+	 * 연관관계 편의 메서드
+	 */
+	public void setStoreTo(Store store) {
+		this.store = store;
 	}
 }
---
+++ src/main/java/com/sparta/nanglangeats/domain/store/repository/StoreImageRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.nanglangeats.domain.store.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sparta.nanglangeats.domain.store.entity.StoreImage;
+
+public interface StoreImageRepository extends JpaRepository<StoreImage, Long> {
+}
