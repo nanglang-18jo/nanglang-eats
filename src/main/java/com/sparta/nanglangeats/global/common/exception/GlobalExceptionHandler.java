061d3eba052f84f55fc885d2dd7d2f1fcb24e3e3
Hut234 <doitdoitdoitdoitdo@gmail.com>
2024-11-14 12:26:10 +0900

    [feat] GlobalExceptionHandler 예외 핸들러 추가

--- src/main/java/com/sparta/nanglangeats/global/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/nanglangeats/global/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.sparta.nanglangeats.global.common.exception;
 
+import static com.sparta.nanglangeats.global.common.exception.ErrorCode.*;
+
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -47,8 +50,8 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDto(builder.toString()));
 	}
 
-	@ExceptionHandler(RuntimeException.class)
-	public ResponseEntity<?> handleException(RuntimeException e) {
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ExceptionDto(e.getMessage()));
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDto(COMMON_INVALID_PARAMETER));
 	}
 }
